4e04e9ee27b3877fd7d0550dc02c567d1d1e0b65
Aman Rajpoot <amanrajpoot59@gmail.com>
2025-04-26 00:07:58 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{ts,tsx}', './pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: [
+    './app/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}'
+  ],
   theme: {
     extend: {
       animation: {
         fadeIn: 'fadeIn 0.5s ease-in-out',
+        spinSlow: 'spin 2s linear infinite',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
         },
-      }
+      },
     },
   },
   plugins: [],
-}
+};
