de93e91c8631fd9d5c3d438c0e00d1b920a43a55
Aman Rajpoot <amanrajpoot59@gmail.com>
2025-04-26 00:32:23 +0530

    Create Waveform.tsx

---
+++ app/components/Waveform.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import './Waveform.css';
+
+const Waveform = () => {
+  const [bars, setBars] = useState(Array(30).fill(0));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBars(bars.map(() => Math.random()));
+    }, 100);
+    return () => clearInterval(interval);
+  }, [bars]);
+
+  return (
+    <div className="waveform-container">
+      {bars.map((bar, i) => (
+        <div
+          key={i}
+          className="bar"
+          style={{ height: `${bar * 100}%` }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Waveform;
